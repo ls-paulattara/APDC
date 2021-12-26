77afaeef264e31e2a6085ab75fdb7e123c737f84
ls-paulattara <paul.attara@lightspeedhq.com>
2021-12-26 13:57:38 -0500

    Finalize report 3

--- src/Util/CreateReportFile.js
+++ src/Util/CreateReportFile.js
@@ -3,8 +3,6 @@ const jspdfautotable = require('jspdf-autotable');
 const Papa = require('papaparse');
 const { compose } = require("recompose");
 
-const doc = new jsPDF()
-
 
 const deliveryZoneList = [ 
   "Zone 1: Montréal-Centre",
@@ -126,6 +124,11 @@ exports.getReport3File = async(data) => {
     },
     "Mail": []
   };
+  // ["Total", totalPrice, count]
+  let deliverytTotal = ["Total", 0,0];
+  let pickupTotal = ["Total", 0,0];
+  let mailTotal = ["Total", 0,0];
+  let grandTotal = ["Grand Total", 0,0];
 
   data.forEach(item => {
     const { id, totalPrice, type, shipmentTitle} = item;
@@ -169,7 +172,7 @@ exports.getReport3File = async(data) => {
     }
   })
 
-  const getPrices = (value) => {
+  const getPrices = (value, type) => {
     Object.keys(value).forEach(function(key) {
      let prices = 0;
      let count = 0;
@@ -177,7 +180,24 @@ exports.getReport3File = async(data) => {
        const { totalPrice } = zone;
        prices += totalPrice;
        count += 1;
+       switch(type){
+         case "delivery":
+           deliverytTotal[1] += totalPrice;
+           deliverytTotal[2] += 1;
+           
+           break;
+        case "pickup":
+          pickupTotal[1] += totalPrice;
+          pickupTotal[2] += 1;
+           break;
+        case "delivery":
+          mailTotal[1] += totalPrice;
+          mailTotal[2] += 1;
+          break;
+       }
      }
+     grandTotal[1] += prices;
+     grandTotal[2] += 1;
      value[key] = [prices, count]; 
    });
   }
@@ -185,42 +205,118 @@ exports.getReport3File = async(data) => {
   for (const [key, value] of Object.entries(dict)) {
     switch(key){
       case "Delivery":
-        getPrices(value);
+        getPrices(value, "delivery");
         break;
       case "Pickup":
-        getPrices(value);
+        getPrices(value, "pickup");
         break;
       case "Mail":
-        getPrices(value);
+        getPrices(value, "mail");
         break;
     }
   }
 
-  // console.log('after', dict["Delivery"]["Montreal"]);
   // for (const [key, value] of Object.entries(dict)) {
-  //   console.log(value)
   // }
+
   console.log('dict', dict);
 
   const deliveryTable = Object.entries(dict["Delivery"]).map(item => [item[0], item[1][0], item[1][1]])
   const pickupTable = Object.entries(dict["Pickup"]).map(item => [item[0], item[1][0], item[1][1]])
+  const mailTable = Object.entries(dict["Mail"]).map(item => [item[0], item[1][0], item[1][1]])
+  console.log(deliveryTable)
+  deliveryTable.push(deliverytTotal);
+  pickupTable.push(pickupTotal);
+  mailTable.push(mailTotal);
 
-  doc.text('Delivery', 14, 10)
+  const doc = new jsPDF()
+
+  var width = doc.internal.pageSize.getWidth() - 28.0222222;
+  
+  // const width1 = 80;
+  // const width2 = 51;
+  // const width3 = 51;
+
+  const width1 = 2 * width / 4;
+  const width2 = 1 * width / 4;
+  const width3 = 1 * width / 4;
+
+  doc.text('Delivery', 14, 10);
 
   doc.autoTable({
     head: [['Zone', 'Total Price', 'Number of Orders']],
     body: deliveryTable,
-    // foot: [["test"]]
-  })
+    columnStyles: {
+      0: {cellWidth: width1},
+      1: {cellWidth: width2},
+      2: {cellWidth: width3}
+    },
+    didParseCell: function (data) {
+      var rows = data.table.body;
+      if (data.row.index === rows.length - 1 && rows.length > 1) {
+          // data.cell.styles.fillColor = [211, 211, 211];
+          data.cell.styles.fontStyle = 'bold';
+      }
+    }
+  });
 
-  doc.text('Pickup', 14, doc.lastAutoTable.finalY + 10)
+  doc.text('Pickup', 14, doc.lastAutoTable.finalY + 10);
  
   doc.autoTable({
-    head: [['Zone', 'Total Price', 'Number of Orders']],
+    head: [['Pickup Point', 'Total Price', 'Number of Orders']],
     body: pickupTable,
     startY: doc.lastAutoTable.finalY + 14,
-  })
-
+    columnStyles: {
+      0: {cellWidth: width1},
+      1: {cellWidth: width2},
+      2: {cellWidth: width3}
+    },
+    didParseCell: function (data) {
+      var rows = data.table.body;
+      if (data.row.index === rows.length - 1 && rows.length > 1) {
+          // data.cell.styles.fillColor = [211, 211, 211];
+          data.cell.styles.fontStyle = 'bold';
+      }
+    }
+  });
+  
+  doc.text('Mail', 14, doc.lastAutoTable.finalY + 10);
+  doc.autoTable({
+    head: [['Mail', 'Total Price', 'Number of Orders']],
+    body: mailTable,
+    startY: doc.lastAutoTable.finalY + 14,
+    columnStyles: {
+      0: {cellWidth: width1},
+      1: {cellWidth: width2},
+      2: {cellWidth: width3}
+    },
+    didParseCell: function (data) {
+      var rows = data.table.body;
+      if (data.row.index === rows.length - 1) {
+          // data.cell.styles.fillColor = [211, 211, 211];
+          data.cell.styles.fontStyle = 'bold';
+      }
+    }
+  });
+  console.log(grandTotal)
+  doc.text('Grand Total', 14, doc.lastAutoTable.finalY + 10);
+  doc.autoTable({
+    head: [['Total', 'Total Price', 'Number of Orders']],
+    body: [grandTotal],
+    startY: doc.lastAutoTable.finalY + 14,
+    columnStyles: {
+      0: {cellWidth: width1},
+      1: {cellWidth: width2},
+      2: {cellWidth: width3}
+    },
+    didParseCell: function (data) {
+      var rows = data.table.body;
+      if (data.row.index === rows.length - 1) {
+          // data.cell.styles.fillColor = [211, 211, 211];
+          data.cell.styles.fontStyle = 'bold';
+      }
+    }
+  });
 
 
   doc.save('table.pdf');
